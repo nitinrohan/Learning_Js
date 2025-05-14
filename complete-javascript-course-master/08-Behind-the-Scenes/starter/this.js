19fb4dc330d2259b90cf101f12df606b66bb548d
Rohan <nitinrohan56@gmail.com>
2025-05-14 18:26:42 -0500

    object references in practise(shallow vs deep copies)

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/this.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/this.js
@@ -61,3 +61,45 @@
 // };
 
 // addArrow(2, 5, 8);
+// console.log((40 * 4) / 5);
+
+///////////////////////////////////////////////////////////////////////////////////
+// object references in practise(shallow vs deep copies)
+const jessica1 = {
+  firstName: 'jessica',
+  lastName: 'williams',
+  age: 27,
+};
+
+const marriedJessica = jessica1; //its not a new memory location but it is just the refereence of the original jessicas location and hence when we change the last name for one variable both of the variables values change.
+marriedJessica.lastName = 'davis';
+
+console.log('before:', jessica1);
+console.log('after:', marriedJessica);
+
+const jessica = {
+  firstName: 'jessica',
+  lastName: 'williams',
+  age: 27, //if we copy only the primitive datatypes like firstName, lastName, age its is called shallow copy. family is anpther object so its a nested object.
+  family: ['rohan', 'bob'],
+};
+
+const jessicaCopy = { ...jessica }; //... is the spread operator which places all the properties of jessica object in this new object
+
+//shallow copy
+// jessicaCopy.LastName = 'davis';
+// console.log(jessica, jessicaCopy);
+
+// jessicaCopy.family.push('mary');
+// jessicaCopy.family.push('john');
+
+// console.log('before:', jessica);
+// console.log('after:', jessicaCopy);
+
+//deep copyor clone
+const jessicaClone = structuredClone(jessica); //its a very important function structuredClone
+jessicaClone.family.push('mary');
+jessicaClone.family.push('john');
+
+console.log('before : ', jessica);
+console.log('after', jessicaClone);
